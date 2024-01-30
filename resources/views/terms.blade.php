1f2598b72d298797111e8c74e89466624dd8b317
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-01-31 01:30:51 +0300

    get ready and go a head

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout')
+@section('content')
+    <h1>Terms</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Nihil nemo maiores,
+        recusandae cupiditate quos, debitis aspernatur ducimus magnam dolores quidem eaque soluta perspiciatis. Voluptatem
+        modi maiores quasi ullam ipsum necessitatibus.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[dashboardController::class,'index'])->name('dashboard');
 
-
-route::get('/dashboard',[dashboardController::class ,'index'])->name('feed');
-
-route::get('/profile',[profileController::class ,'index'])->name('userProfile');
+Route::get('/terms',function(){return view('terms');})->name('terms');
