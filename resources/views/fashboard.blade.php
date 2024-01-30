0a43e0b653bbf116fa99184e658893565846d135
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-01-30 19:58:24 +0300

    init commit

---
+++ resources/views/fashboard.blade.php
@@ -0,0 +1,15 @@
+{{-- bootstrp-twitter-clone-template --}}
+
+
+{{-- download it --}}
+{{-- learn formated --}}
+{{-- make layout folder with two foiles one for glaypout and one for nav file --}}
+{{-- make terms view nd dashboard view  --}}
+{{-- make name of the app and repalce in brand and titile nnd make rooute  --}}
+
+
+{{-- deal with database  --}}
+{{-- mysql -u root; --}}
+{{-- create database ideasdb --}}
+{{-- run php artisan migrate--}}
+{{-- use odeasdb and then run the dirst and make show tables --}}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\profilecontroller;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+route::get('/dashboard',[dashboardController::class ,'index'])->name('feed');
+
+route::get('/profile',[profileController::class ,'index'])->name('userProfile');
