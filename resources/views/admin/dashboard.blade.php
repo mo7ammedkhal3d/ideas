a354061bada61b68ee73b496a68417a2e835f3ac
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-02-09 02:31:04 +0300

    roles and permision P1

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+@section('content')
+<h1>Admin Panal</h1>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\IdeaLikeController;
+use App\Http\Controllers\admin\dashboardController as AdminDashboardController;
 use App\Http\Controllers\profilecontroller;
 use App\Http\Controllers\UserController;
 use GuzzleHttp\Middleware;
@@ -74,6 +75,9 @@ Route::post('idea/{idea}/like', [IdeaLikeController::class, 'like'])
 Route::post('idea/{idea}/unlike', [IdeaLikeController::class, 'unlike'])
     ->name('idea.unlike')
     ->middleware('auth');
+
+
+Route::get('/admin', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware('auth');
 
 Route::get('/terms', function () {
     return view('terms');
