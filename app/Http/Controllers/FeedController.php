9b6b528f7a41589fdc614bdc1465870c58bdd97c
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-02-09 01:56:03 +0300

    add feed P2

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\Idea;
 class FeedController extends Controller
 {
-    
+    public function __invoke(Request $request)
+    {
+
+        $followingsIds = auth()->user()->followings()->pluck('user_id');
+
+        $ideas = Idea::whereIn('user_id',$followingsIds)->latest();
+
+        if (request()->has('search')) {
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search', '') . '%');
+        }
+
+        return view('dashboard', ['ideas' => $ideas->paginate(5)]);
+    }
 }
