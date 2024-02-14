3cb9fd046ecc4d61b30897b86c545f4c61dad384
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-02-14 22:06:40 +0300

    update Unsure UserIsAdmin file

--- app/Http/Middleware/EnsureUserIsAdmin.php
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -13,7 +13,7 @@ class EnsureUserIsAdmin
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+     */  #mk
     public function handle(Request $request, Closure $next): Response
     {
 
