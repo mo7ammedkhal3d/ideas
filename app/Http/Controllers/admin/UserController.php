6874d4b60a91596a04b8d5567b50f6dcac3ed19a
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-02-26 00:36:11 +0300

    CRUD Admin users

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Usercontroller extends Controller
 {
     public function index(){
-        return redirect()->route('admin.users.index');
+
+
+        $users = User::latest()->paginate(5);
+
+
+        return view('admin.users.index',compact('users'));
     }
 }
